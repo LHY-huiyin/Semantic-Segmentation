3f4b85ff6cfa72ade5bb65981d76a81d16627990
DaYinYi <luohuiyin0524@163.com>
2022-06-15 22:01:55 +0800

    "ghostnet"

--- modeling/aspp_ghostnet.py
+++ modeling/aspp_ghostnet.py
@@ -38,6 +38,8 @@ class ASPP(nn.Module):
             inplanes = 512
         elif backbone == 'mobilenet':
             inplanes = 320
+        elif backbone == 'ghostnet':
+            inplanes = 160
         else:
             inplanes = 2048  # backbone = 'resnet'
 
