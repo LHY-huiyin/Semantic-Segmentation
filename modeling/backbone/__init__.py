f54a04a57154bd171e8257fce4065d5a80d170ff
DaYinYi <luohuiyin0524@163.com>
2022-06-22 16:22:26 +0800

    “首先将backbone的输出改成[x1,x2,x3,x4]”“将deeplab的结构调整，采用点流模块“”PPM代替aspp”

--- modeling/backbone/__init__.py
+++ modeling/backbone/__init__.py
@@ -1,9 +1,9 @@
-from modeling.backbone import resnet, xception, drn, mobilenet, ghostnet_rectify, resnetxt_cifar, resnetxt, mobilenet_origin, ghostnet_origin, resnet50
+from modeling.backbone import resnet, xception, drn, mobilenet, ghostnet_rectify, resnetxt_cifar, resnetxt, mobilenet_origin, ghostnet_origin, resnet50, resnet50_pointflow
 
 def build_backbone(backbone, output_stride, BatchNorm):  # 'resnet' 16
     if backbone == 'resnet':  # 16
         # return resnet.ResNet101(output_stride, BatchNorm)
-        return resnet50.ResNet50(output_stride, BatchNorm)
+        return resnet50_pointflow.ResNet50(output_stride, BatchNorm)
         # return resnetxt.resnext101()
     elif backbone == 'xception':
         return xception.AlignedXception(output_stride, BatchNorm)
