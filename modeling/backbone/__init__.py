c0aeb5872abc1bc7d61bcf580f5d28730df161c5
DaYinYi <luohuiyin0524@163.com>
2022-07-13 20:43:47 +0800

    "在resnet101的基础上,重新跑之前的代码"

--- modeling/backbone/__init__.py
+++ modeling/backbone/__init__.py
@@ -9,8 +9,8 @@ def build_backbone(backbone, output_stride, BatchNorm):  # 'resnet' 16
         # return resnetxt.resnext101()
         # return resnet50_deeplabunet.ResNet50(output_stride, BatchNorm)  # 获取5个输出
         # return resnet50_bifpn.ResNet50(output_stride, BatchNorm)
-        # return resnet101_msapp.ResNet101(output_stride, BatchNorm)  # 获取4各输出
-        return resnet101_bifpn.ResNet101(output_stride, BatchNorm)
+        return resnet101_msapp.ResNet101(output_stride, BatchNorm)  # 获取4各输出
+        # return resnet101_bifpn.ResNet101(output_stride, BatchNorm)
     elif backbone == 'xception':
         return xception.AlignedXception(output_stride, BatchNorm)
     elif backbone == 'drn':
