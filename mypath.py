bb3c9c6cda2bd7f761c538f252be117c46df075e
DaYinYi <luohuiyin0524@163.com>
2022-07-23 11:50:32 +0800

    "改用公共数据集来看实验效果"

--- mypath.py
+++ mypath.py
@@ -6,10 +6,12 @@ class Path(object):
         if dataset == 'pascal':
             # return '/root/autodl-tmp/loveda_a/'  # AutoDL云服务器跑
             # return 'G:\\loveda_aft\\'
-            return 'C:\\Remote sensing semantic segmentation\\loveda_a\\'  #'G:\\Loveda_a\\'  数据集大小为512*512
+            # return 'C:\\Remote sensing semantic segmentation\\loveda_a\\'  #'G:\\Loveda_a\\'  数据集大小为512*512
             # return 'C:\\Remote sensing semantic segmentation\\loveda_last\\'  # 错误的设置：训练集348 验证集512
             # return 'X:\\luo\\loveda_a\\'  # 3080师弟电脑的路径
             # return 'C:\\luo\\loveda_a\\'  # 3090服务器的路径
+
+            return 'H:\\datasets\\VOCPotsdam\\'  # 改用公共数据集
         # elif dataset == 'sbd':
         #     return '/path/to/datasets/benchmark_RELEASE/'  # folder that contains dataset/.
         # elif dataset == 'cityscapes':
