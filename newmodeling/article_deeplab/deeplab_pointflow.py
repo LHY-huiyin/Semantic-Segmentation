76d1df0ffdf39d08d0ea1e956fc4b945fcfe8720
DaYinYi <luohuiyin0524@163.com>
2022-09-16 16:40:54 +0800

    "Pointflow:因为参数有aspp，不能改"

--- newmodeling/article_deeplab/deeplab_pointflow.py
+++ newmodeling/article_deeplab/deeplab_pointflow.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from newmodeling.sync_batchnorm.batchnorm import SynchronizedBatchNorm2d
-# from newmodeling.aspp import *
+from newmodeling.aspp import *
 from newmodeling.decoder import build_decoder
 from newmodeling.backbone import build_backbone
 from newmodeling.articlecode.PointFlow import PointFlowModuleWithMaxAvgpool
@@ -185,8 +185,8 @@ class DeepLab(nn.Module):
             BatchNorm = nn.BatchNorm2d  # 数据的归一化处理   y = \frac{x - \mathrm{E}[x]}{ \sqrt{\mathrm{Var}[x] + \epsilon}} * \gamma + \beta
 
         self.backbone = build_backbone(backbone, output_stride, BatchNorm)  # 'resnet' 16 BatchNorm2d
-        # self.aspp = build_aspp(backbone, output_stride, BatchNorm)
-        # self.decoder = build_decoder(num_classes, backbone, BatchNorm)
+        self.aspp = build_aspp(backbone, output_stride, BatchNorm)
+        self.decoder = build_decoder(num_classes, backbone, BatchNorm)
 
         self.freeze_bn = freeze_bn
         self.head = UperNetAlignHeadMaxAvgpool(pointflow_chs,  num_class=num_classes, norm_layer=BatchNorm,
